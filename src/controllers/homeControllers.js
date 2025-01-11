aedc982e940ad6e6866df26fa523ee599858ff98
Minh_Tung <95851147+minh-tung-76-23@users.noreply.github.com>
2025-01-12 00:15:44 +0700

    Configure to connect directly to the database

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,5 +1,25 @@
-let homePage = (req, res) => {
-  return res.render("homePage.ejs");
+import db from "../models/index";
+let homePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    if (data.length > 0) {
+      console.log("--------------------------------");
+      console.log(data);
+      console.log("--------------------------------");
+    } else {
+      console.log("No data found in the database");
+      return res.render("homePage.ejs", {
+        data: "No data found in the database",
+      });
+    }
+
+    // Convert the data to JSON and send it to the client
+    return res.render("homePage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 let aboutPage = (req, res) => {
